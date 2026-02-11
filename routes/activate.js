a1eaa35b7e04318effc247def8b6bfdb95529ee7
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2026-02-11 18:40:18 +0530

    qdrant alive"

--- routes/activate.js
+++ routes/activate.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import { chat } from "../utils/bot.js";
+import { retrive } from "../utils/vectorstore.js";
 
 const activateRouter = Router();
 
 activateRouter.get('/',
     async (req,res)=>{
         // console.log("started");
-        fetch('https://f594cffa-e112-4e71-8b9e-b3bea28f3539.europe-west3-0.gcp.cloud.qdrant.io')
         res.send("Hi Bot backend here")
+    }
+)
+activateRouter.get('/qdrant',
+    async (req,res)=>{
+        // console.log("started");
+        const resawait = await retrive("Aaditya");
+        res.send("Qdrant Alive?"+ resawait)
     }
 )
 
